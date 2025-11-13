62d6fea6df2a827e670de417691bc9d61b8a1376
pawankumar651454-maker <pawankumar651454@gmail.com>
2025-11-13 10:48:32 +0530

    Update AffordableRoomPage.js

--- src/Components/New/AffordableRoomPage.js
+++ src/Components/New/AffordableRoomPage.js
@@ -84,7 +84,7 @@ const AffordableRoomPage = ({phone}) => {
 
       <div className="relative w-full sm:w-[80%] lg:w-1/2 h-full flex justify-center items-center">
         <div className="h-[70%] w-full md:w-[70%] rounded-2xl overflow-hidden">
-          <img src="/New/room.avif" className="h-full w-full object-cover" />
+          <img alt="a" src="/New/room.avif" className="h-full w-full object-cover" />
         </div>
         <div className="absolute text-white flex gap-6 w-[80%] md:w-[65%] lg:w-1/2 p-6 lg:p-10 bg-[#a67c52] top-[78%] lg:top-[60%] lg:right-[60%]">
           <div>
