a16c2824865ef9652f2e4106238889ddd9a3d3d7
pawankumar651454-maker <pawankumar651454@gmail.com>
2025-11-13 10:56:37 +0530

    Update BirlaDharamshala.js

--- src/Components/BirlaDharamshala.js
+++ src/Components/BirlaDharamshala.js
@@ -34,7 +34,7 @@ export default function BirlaDharamshala() {
 
       <div className="mt-8">
         <h4 className="text-xl font-medium mb-2">Location:</h4>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d82372.58550154182!2d82.15441324630828!3d26.791719441310143!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399a07ecdf27cc41%3A0x7c9bd8b0b273a80f!2sBirla%20Dharamshala!5e0!3m2!1sen!2sus!4v1738317392182!5m2!1sen!2sus" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" className="w-full h-[200px] sm:h-[400px]"></iframe>
+        <iframe title="Map showing Birla Dharamshala location" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d82372.58550154182!2d82.15441324630828!3d26.791719441310143!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399a07ecdf27cc41%3A0x7c9bd8b0b273a80f!2sBirla%20Dharamshala!5e0!3m2!1sen!2sus!4v1738317392182!5m2!1sen!2sus" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" className="w-full h-[200px] sm:h-[400px]"></iframe>
       </div>
     </div>
   );
